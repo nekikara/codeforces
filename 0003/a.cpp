4b09899489bbca886d6b7b5c33aee408cbb66947
nekikara <a.nek1427@gmail.com>
2015-11-19 01:17:28 +0900

    3_a shortest path of the king

--- 0003/a.cpp
+++ 0003/a.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <vector>
 using namespace std;
-map<char, int> R
-{
-    {'a', 0},
-    {'b', 1},
-    {'c', 2},
-    {'d', 3},
-    {'e', 4},
-    {'f', 5},
-    {'g', 6},
-    {'h', 7}
-};
-
-map<int, int> C {
-    {7, 0},
-    {6, 1},
-    {5, 2},
-    {4, 3},
-    {3, 4},
-    {2, 5},
-    {1, 6},
-    {0, 7},
-}
-
-string switchString(string str) {
-    char f = str[0];
-    char s = str[1];
-    str[1] = f;
-    str[0] = s;
-    return str;
-}
 
 int main() {
     string piece;
     string goal;
     cin >> piece;
     cin >> goal;
+    int s_x = piece[0] - 0;
+    int s_y = (int)piece[1];
+    int t_x = goal[0] - 0;
+    int t_y = (int)goal[1];
+    int dx = s_x - t_x;
+    int dy = s_y - t_y;
 
-    string s_piece = switchString(piece);
-    string g_piece = switchString(goal);
-
-    
-
+    vector<string> paths;
+    while (!(dx == 0 && dy == 0)) {
+        string c = "", r = "";
+        if (dx < 0) {
+            dx += 1;
+            c ="R";
+        } else if (dx > 0) {
+            dx -= 1;
+            c= "L";
+        }
+        if (dy < 0) {
+            dy += 1;
+            r = "U";
+        } else if (dy > 0) {
+            dy -= 1;
+            r = "D";
+        }
+        paths.push_back(c + r);
+    }
+    cout << paths.size() << endl;
+    for (int i=0; i < paths.size(); i++) {
+        cout << paths[i] << endl;
+    }
     return 0;
 }
