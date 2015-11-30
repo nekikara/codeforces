c26d7f5f67b723947d904d1587849ccc2133120b
nekikara <a.nek1427@gmail.com>
2015-12-01 00:20:34 +0900

    14_a letter

--- 0014/a.cpp
+++ 0014/a.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 int main() {
-    /*
-    string str = "..........";
-    int l = str.find("*");
-    int r = str.rfind("*");
-    cout << l <<endl;
-    cout << r << endl;
-    */
-
-
     int n, m;
     cin >> n >> m;
-    int l = m / 2, r = m / 2;
+    int l = m, r = 0;
     vector<string> S;
+    int b = 0;
+    int u = -1;
     for (int i=0; i<n; i++){
         string s;
         cin >> s;
         int le = s.find("*");
         int ri = s.rfind("*");
-        if (le == -1 && ri == -1 && (i == 0 || i == n - 1)) continue;
+        if (0 <= le && 0 <= i && -1 == u) u = i;
         if (0 <= le && le < l) l = le;
         if (r < ri && ri <= m) r = ri;
+        if (le != -1 || ri != -1) b = i;
         S.push_back(s);
     }
 
-    for(int i=0; i<S.size(); i++) {
-        cout << S.at(i).substr(l,r-l+1) << endl;
+
+    if (0 <= u) {
+        for(int i=u; i<b+1; i++) {
+            cout << S.at(i).substr(l,r-l+1) << endl;
+        }
     }
 
     return 0;
